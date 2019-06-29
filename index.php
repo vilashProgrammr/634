6a26b78d5026b530953b33856c0137792dea5d9a
candidate <candidate@programmr.com>
2019-06-29 04:36:59 +0000

    FINISH TEST

--- index.php
+++ index.php
@@ -16,6 +16,9 @@
  */
 define('DRUPAL_ROOT', getcwd());
 
+mysql_connect("SAdsad","ASD");
+
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 menu_execute_active_handler();
+//DJ@PROGRAMMR.COM
